1e47b1e15302e4deb3c14204d8bb4821490280ce
Malik Abdul Wahab <malikwahab34@gmail.com>
2023-10-01 18:15:09 +0500

    React + Tailwind css with Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center mt-12 text-4xl font-bold italic">
+        Hello, React + TailwindCSS using Vite
+      </h1>
+    </>
+  );
+}
+
+export default App;
